7e43032b4fee5c8d7cee1e094d8c9ee38220349c
lmarriottc <100027981+lmarriottc@users.noreply.github.com>
2023-08-20 09:39:05 -0500

    solved

---
+++ ejercicio_profundizacion/pa-8_6C1.py
@@ -0,0 +1,34 @@
+# IMPORTANTE: NO borrar los comentarios o variables
+tipo = "desafio"
+
+# Alumno:
+# 1) Forkear el repositorio de clase
+repositorio_clase = "https://github.com/InoveAlumnos/clustering_python"
+
+# 2) Descargar el repositorio en su PC
+
+# 3) Realizar el desafio que se encuentra
+# dentro de la carpeta ejercicio_profundizacion llamado:
+nombre_del_desafio = "1_segmentacion_paises.ipynb"
+
+# Para eso recomendamos subir el archivo de desafio
+# a colab y trabajarlo allí
+
+# 4) Una vez terminado el desafio en colab, descargarlo
+# a su PC y subir ese archivo a github, importante:
+# - Debe subir este archivo a la carpeta ejercicio_profundizacion
+# reemplazando el original
+# - Recomendamos ver los instructivos de github Web para
+# llevar a cabo esta tarea
+# - Si todo funcionó correctamente, podrá ver en github
+# si archivo de notebook con los cambios que usted introdujo
+
+# 5) En la variable "mi_repositorio" indique cual es
+# su repositorio (la URL de su repositorio dónde subió el archivo)
+# ejemplo:
+# mi_repositorio = "https://github.com/miusuario/...."
+
+mi_repositorio = "https://github.com/lmarriottc/clustering_python"
+
+
+print("fin")
